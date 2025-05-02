15616d48c5f511273cd286e8fb22ddb0971a68f2
XuanHuy <23110102@student.hcmute.edu.vn>
2025-05-02 20:20:52 +0700

    update NhanVien , DoanhThuDB

--- DAL/DBNhanVien.cs
+++ DAL/DBNhanVien.cs
@@ -23,16 +23,53 @@ namespace DAL
         public List<DanhSachNhanVien> LayDsNhanVien()
         {
             List<DanhSachNhanVien> list = new List<DanhSachNhanVien>();
-            QuanLyTiemNetEntities db = new QuanLyTiemNetEntities();
-            list = db.DanhSachNhanViens.ToList();
+            list = QuanLyTiemNetEntities.Instance.DanhSachNhanViens.ToList();
             return list;
         }
         public List<DanhSachNhanVien> TimNhanVien(string searchText)
         {
             List<DanhSachNhanVien> list = new List<DanhSachNhanVien>();
-            QuanLyTiemNetEntities db = new QuanLyTiemNetEntities();
-            list = db.DanhSachNhanViens.Where(x => x.HoTen.Contains(searchText)).ToList();
+            list = QuanLyTiemNetEntities.Instance.DanhSachNhanViens.Where(x => x.MaNV.ToString().Contains(searchText) || x.HoTen.Contains(searchText)).ToList();
             return list;
+        }
+        public bool ThemNhanVien(NHANVIEN nv)
+        {
+            try
+            {
+                QuanLyTiemNetEntities db = new QuanLyTiemNetEntities();
+                db.sp_ThemNhanVien(nv.HoTen, nv.SDT, nv.DiaChi, nv.GioiTinh, nv.NgaySinh);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public bool XoaNhanVien ( int id) {
+            try
+            {
+                QuanLyTiemNetEntities.Instance.sp_XoaNhanVien(id);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return false;
+            }
+        }
+        public bool SuaNhanVien(NHANVIEN nv)
+        {
+            try
+            {
+                QuanLyTiemNetEntities.Instance.sp_SuaNhanVien(nv.MaNV, nv.HoTen, nv.DiaChi, nv.GioiTinh, nv.NgaySinh);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return false;
+            }
         }
 
 
